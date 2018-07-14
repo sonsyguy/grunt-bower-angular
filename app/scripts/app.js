619bd7b66b66a5570187a26ce824f26cb433a06a
helife <15900239861@163.com>
2018-07-14 16:56:18 +0800

    the first yeoman grunt bower angular item

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name heheApp
+ * @description
+ * # heheApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('heheApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
